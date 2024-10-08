05fc48d96d0ac2786fd0414c38d1990954b943d0
od <od@sweettracker.co.kr>
2024-10-08 13:23:08 +0900

    feat: resource API 수정

--- src/main/java/com/sweettracker/k8stestapp/controller/ResourceController.java
+++ src/main/java/com/sweettracker/k8stestapp/controller/ResourceController.java
@@ -19,7 +19,7 @@ public class ResourceController {
 
     @GetMapping("/resource")
     public String getData() {
-        String data = "hello - " + username;
+        String data = "hello - v2 " + username;
         log.info(data);
         return data;
     }
